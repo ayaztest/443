b3563776b348f5e139c7fbfaf796d157c5d12695
ayaztest <arthurthehonestone@gmail.com>
2023-02-06 09:31:26 +0500

    without contract data

--- pages/index.tsx
+++ pages/index.tsx
@@ -154,31 +154,7 @@ const getNFTNames = (nfts: NFT[]) => {
     setTotaltwo('')
     
   }
-  useEffect(() => {
-  if (!address) {
-    return;
-  }
 
-  const getOwnedNFTNames = async () => {
-    try {
-      if (firstContract) {
-        const nfts = await firstContract.getOwned(address);
-        const ownedNFTNames = nfts.map((nft) => nft.metadata.name.split(" #")[1]);
-        setOwnedNFTNames(ownedNFTNames);
-      }
-      
-      if (secondContract) {
-        const nfts = await secondContract.getOwned(address);
-        const ownedNFTNamestwo = nfts.map((nft) => nft.metadata.name.split(" #")[1]);
-        setOwnedNFTNamestwo(ownedNFTNamestwo);
-      }
-    } catch (error) {
-      console.error("Failed to get owned NFT names", error);
-    }
-  };
-
-  getOwnedNFTNames();
-}, [address, firstContract, secondContract]);
   
   useEffect(() => {
     if (address) {
